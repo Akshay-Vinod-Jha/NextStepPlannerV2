f7948c040ecf29b637e2c2adf0843bb36cdaf184
Om Gholap <omgholap051@gmail.com>
2025-10-05 16:55:14 +0530

    O auth keys update

--- Backend/index.js
+++ Backend/index.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(passport.initialize());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","http:/localhost:5174"],
     credentials: true,
   })
 );
